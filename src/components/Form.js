ca13fc9431328a0e518a774e7548cffe8ade7f64
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-24 06:33:26 -0700

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,83 @@
+// src/components/ParentComponent
 import React from 'react';
+import Form from './Form'
+import DisplayData from './DisplayData'
 
-class Form extends React.Component {
+class ParentComponent extends React.Component {
   state = {
-    firstName: "John",
-    lastName: "Henry"
+    firstName: "",
+    lastName: "",
   }
 
-  handleFirstNameChange = event => {
+  handleChange = event => {
     this.setState({
-      firstName: event.target.value
-    })
-  }
-
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
+      [event.target.name]: event.target.value
     })
   }
 
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
+      <div>
+        <Form
+          formData={this.state}
+          handleChange={this.handleChange}
+        />
+        <DisplayData formData={this.state} />
+      </div>
+    )
+  }
+}
+
+export default ParentComponent;
+// src/components/Form
+import React from 'react';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            name="firstName"
+            onChange={event => this.props.handleChange(event)}
+            value={this.props.formData.firstName}
+          />
+          <input
+            type="text"
+            name="lastName"
+            onChange={event => this.props.handleChange(event)}
+            value={this.props.formData.lastName} />
+        </form>
+      </div>
     )
   }
 }
 
 export default Form;
+// src/components/DisplayData.js
+import React from 'react';
+
+class DisplayData extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.formData.firstName}</h1>
+        <h1>{this.props.formData.lastName}</h1>
+      </div>
+    )
+  }
+}
+
+export default DisplayData
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ParentComponent from './components/ParentComponent';
+
+ReactDOM.render(
+  <div>
+    <ParentComponent/>
+  </div>,
+  document.getElementById('root')
+);
